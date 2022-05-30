aef5333919f744a46399cc0fc14cd2c8e952fd1c
Wouter Gazendam <wouter@ag5.com>
2022-05-30 19:22:21 +0200

    Added FilterSlice

--- collectiontools.go
+++ collectiontools.go
@@ -1,5 +1,6 @@
 package gofunk
 
+// MapSlice does a map() on a Slice; producing a Slice
 // #(a b c) collect: fn
 func MapSlice[E any, R any](sl []E, fn func(E) R) []R {
 	coll := make([]R, len(sl))
@@ -9,11 +10,24 @@ func MapSlice[E any, R any](sl []E, fn func(E) R) []R {
 	return coll
 }
 
+// MapRange does a map on all integers between start and stop; producing a Slice
 // (1 to: 100) collect: fn
 func MapRange[R any](start int, stop int, fn func(int) R) []R {
 	coll := make([]R, stop-start)
 	for i := start; i < stop; i++ {
 		coll[i] = fn(i)
+	}
+	return coll
+}
+
+// FilterSlice does a filter() on a Slice; producing a Slice
+// #(a b c) select: fn
+func FilterSlice[E any](sl []E, fn func(E) bool) []E {
+	coll := make([]E, 0)
+	for _, e := range sl {
+		if fn(e) {
+			coll = append(coll, e)
+		}
 	}
 	return coll
 }
