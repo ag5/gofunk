67e46747acbd8604cab85c0e53e65fbfea54e69c
Wouter Gazendam <wouter@ag5.com>
2022-06-13 16:30:29 +0200

    WIP joins

--- funk/collectiontools.go
+++ funk/collectiontools.go
@@ -73,10 +73,10 @@ func AppendIfAbsent[E any](sl []E, elem E) []E {
 	return append(sl, elem)
 }
 
-func AppendIfNotEQ[E any](sl []E, elem E, cmpfn func(a, b E) bool) []E {
+func AppendIfNotEQ[E any](sl []E, elem E, cmp func(a, b E) bool) []E {
 	found := false
 	for _, e := range sl {
-		if cmpfn(e, elem) {
+		if cmp(e, elem) {
 			found = true
 		}
 	}
@@ -90,4 +90,64 @@ func CopySlice[E any](sl []E) []E {
 	var cp []E
 	copy(cp, sl)
 	return cp
+}
+
+func EqualsMap[K comparable, V any](mapA map[K]V, mapB map[K]V, cmp func(a, b V) bool) bool {
+
+	if len(mapA) != len(mapB) {
+		return false
+	}
+
+	for k, v := range mapA {
+		ov, found := mapB[k]
+		if !found {
+			return false
+		}
+		if !cmp(v, ov) {
+			return false
+		}
+	}
+	return true
+}
+
+func EqualsSlice[V any](sliceA []V, sliceB []V, cmp func(a, b V) bool) bool {
+
+	if len(sliceA) != len(sliceB) {
+		return false
+	}
+
+	for idx, v := range sliceA {
+		ov := sliceB[idx]
+		if !cmp(v, ov) {
+			return false
+		}
+	}
+	return true
+}
+
+func SliceDetect[V any](sl []V, test func(e V) bool) (*V, bool) {
+	for _, v := range sl {
+		if test(v) {
+			return &v, true
+		}
+	}
+	return nil, false
+}
+
+func SliceAnySatisfy[V any](sl []V, test func(e V) bool) bool {
+	for _, v := range sl {
+		if test(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func MapAnySatisfy[K comparable, V any](mp map[K]V, test func(e V) bool) bool {
+	for _, v := range mp {
+		if test(v) {
+			return true
+		}
+	}
+	return false
 }
